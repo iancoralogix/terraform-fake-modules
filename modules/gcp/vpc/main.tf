30806f004618f7465a25425373ea57e6d94dd684
iangrunt <ian@gruntwork.io>
2022-08-07 22:09:17 -0400

    add gcp

--- modules/gcp/vpc/main.tf
+++ modules/gcp/vpc/main.tf
@@ -18,9 +18,12 @@ locals {
     project = local.project
   }
 
+  region_map = zipmap(data.google_compute_regions.available.names, local.available_region_shortnames)
+
   #TODO this is really just winging it
   indexes_used_to_select_fake_subnets = distinct(split("", substr(replace(local.string_used_for_fake_ids, "/[abcdefg]/", ""), 0, 14)))
-  subnet_regions                      = [for i in local.indexes_used_to_select_fake_subnets : element(local.available_region_shortnames, i * i + 7)]
+  #  subnet_regions                      = [for i in local.indexes_used_to_select_fake_subnets : element(local.available_region_shortnames, i * i + 7)]
+  subnet_regions = [for i in local.indexes_used_to_select_fake_subnets : element(data.google_compute_regions.available.names, i * i + 7)]
   #TODO this could be cleaned up, will produce /24's, could be done a number of ways
   subnet_cidr_blocks = [for s in range(length(local.subnet_regions)) : cidrsubnet("10.0.0.0/8", 16, s)]
   project            = data.google_project.this.name
@@ -30,7 +33,7 @@ locals {
     gateway_address = cidrhost(local.subnet_cidr_blocks[i], 1),
     network         = "https://www.googleapis.com/compute/v1/projects/${local.project}/global/networks/${local.vpc.name}"
     project         = local.project
-    id              = "projects/${local.project}/regions/${k}/subnetworks/${join("-", [var.namespace, var.environment, "subnet"])}"
+    id              = "projects/${local.project}/regions/${k}/subnetworks/${join("-", [var.namespace, var.environment, local.region_map[k], "subnet"])}"
 
   }]
 }
